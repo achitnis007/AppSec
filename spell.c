e0406e0dbb4c705cf324d52cde238adbdc49983a
achitnis007 <abhijit.chitnis@gmail.com>
2019-09-19 00:49:45 -0400

    Completed coding - now compile & test

--- spell.c
+++ spell.c
@@ -13,6 +13,8 @@
 #include <ctype.h>
 #include "dictionary.h"
 
+#define LINEBUFF 2048
+
 // in dictionary.h
 // maximum length for a word
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
@@ -50,6 +52,34 @@
  **/
 int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[])
 {
+    char line[LINEBUFF];
+    char *ip_word;
+    int num_misspelled = 0;
+    
+    misspelled = (char *) malloc(MAX_MISSPELLED * sizeof(char *));
+    if (misspelled == NULL){
+        // initialize all string pointers to NULL
+	for (int i=0; i < MAX_MISSPELLED; i++){
+	    misspelled[i] = NULL;
+	}
+    }
+    else
+        exit(1);
+
+    while (fgets(line, LINEBUFF, fp) ! = NULL){
+        ip_word = strtok(line, " ");
+	while (ip_word != NULL){
+            ip_word = strtok(NULL, " ");
+	    if (check_word(ip_word, hashtable) == FALSE) {
+                // add this word to misspelled array    
+		misspelled[num_misspelled] = (char *) malloc((strlen(ip_word)+1) * sizeof(char));
+	        num_misspelled++;
+
+            }
+	}
+    }
+
+    return num_misspelled;
 }
 
 
@@ -71,7 +101,8 @@ int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[])
  **/
 bool check_word(const char* word, hashmap_t hashtable[])
 {
-    int alpha_cnt = 0, num_cnt = 0;
+    int alpha_cnt = 0, num_cnt = 0, punct_cnt = 0;
+    bool punct_begin = FALSE, punct_end = FALSE;
     char chkword[LENGTH+1], chkword_lower[LENGTH+1];
 
  
@@ -92,6 +123,11 @@ bool check_word(const char* word, hashmap_t hashtable[])
 	else if (word[i] >= '0' && word[i] <= '9'){
 	    num_cnt++;
 	}
+	else if (ispunct(word[i])){
+	    if (i == 0) punct_begin = TRUE;
+	    if (i == word_len-1) punct_end = TRUE;
+	    punct_cnt++;
+	}
 	else if (word[i] < 32 || word[i] == 127){
 	    return FALSE;
 	}
@@ -107,15 +143,16 @@ bool check_word(const char* word, hashmap_t hashtable[])
 
     // check if first char is a punctuation mark - if so, copy from 2nd char to end
     // else copy the entire word to chkword
-    if ( (word[0] >= 33 && word[0] <= 47) || (word[0] >= 91 && word[0] <= 96) || (word[0] >= 123 && word[0] <= 126) )
+    if (punct_begin == TRUE)
 	strcpy(chkword, word+1);
     else
 	strcpy(chkword, word);
 
     // if the last char of word is punc, truncate chkword string to eliminate the last punc mark
-    if ( (word[word_len-1] >= 33 && word[word_len-1] <= 47) || (word[word_len-1] >= 91 && word[word_len-1] <= 96) || (word[word_len-1] >= 123 && word[word_len-1] <= 126) ){
+    if (punct_end == TRUE){
 	int chkword_len = strlen(chkword);
-	chkword[chkword_len-1] = '\0';  // truncate last char of chkword since it's a punc
+	// null terminate string
+	chkword[chkword_len-1] = '\0';  
     }
 
     int bucket = hash_function(chkword);
@@ -226,3 +263,33 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
  * Already implemented in dictionary.c
  **/
 // int hash_function(const char* word);
+
+int main(int argc, char** argv)
+{
+    // declare hashtable_t hashtable[]
+    // declare char *misspelled[] to store misspelled words
+    // open the input file - pass FILE * to check_words
+    // load dictionary
+    // call check_words
+
+    hashtable_t hashtable[HASH_SIZE];
+    char * misspelled[MAX_MISSPELLED];
+    int num_misspelled = 0;
+    char * dictionary_file = "./wordlist.txt";
+
+    fp = fopen("./input_file.txt", "r");
+    if (fp == NULL){
+	printf("Could not open input_file.txt to be spell checked\n");
+	exit(1);
+    }
+
+    if (load_dictionary(dictionary_file, hashtable) == TRUE){
+	num_misspelled = check_words(fp, hashtable, misspelled);
+    }
+    else{
+	printf("Failed to load dictionary file - exiting without checking input file\n"); 
+	exit(1);
+    }
+
+    exit(0);
+}
